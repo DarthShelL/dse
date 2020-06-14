5e85c05b3fdf0647ff604f46fe1f345c1b2584ab
DarthShelL <shellnosferatu@gmail.com>
2020-06-14 12:11:38 +0300

    delete logs

--- src/dse/engine/io/Input.java
+++ src/dse/engine/io/Input.java
@@ -1,5 +1,23 @@
 package dse.engine.io;
 
-public class Input {
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWCursorPosCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
+import org.lwjgl.glfw.GLFWMouseButtonCallback;
 
+public class Input {
+    private GLFWKeyCallback keyboard;
+    private GLFWCursorPosCallback mouseMove;
+    private GLFWMouseButtonCallback mouseButtons;
+
+    private boolean[] keys = new boolean[GLFW.GLFW_KEY_LAST];
+
+    public void Input() {
+        keyboard = new GLFWKeyCallback() {
+            @Override
+            public void invoke(long window, int key, int scancode, int action, int mods) {
+
+            }
+        };
+    }
 }
